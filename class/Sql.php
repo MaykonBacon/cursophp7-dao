29eb8f5b433fd398a1d42df6c45b057631278d6d
Maykon Bacon <maykon_bacon@yahoo.com.br>
2017-11-16 18:41:05 -0200

    criamos a classe usuario

--- class/Sql.php
+++ class/Sql.php
@@ -7,14 +7,14 @@ class Sql extends PDO {
 		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
 	}
 	//Setar vários parâmentros
-	private function setParams($statment, $parameters = array()){
+	private function setParams($statement, $parameters = array()){
 		foreach ($parameters as $key => $value) {
-			$this->setParam($key, $value);
+			$this->setParam($statement, $key, $value);
 		}
 	}
 	//Setar apenas um parâmetro
-	private function setParam($statment, $key, $value){
-		$statment->bindParam($key, $value);
+	private function setParam($statement, $key, $value){
+		$statement->bindParam($key, $value);
 	}
 	//Executar comandos no BD (Query) - Consulta
 	public function query($rawQuery, $params = array()){
--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
 //Index - requer o config e executa o SQL já fazendo uma consulta
 require_once("config.php");
-$sql = new Sql();
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+$root = new Usuario();
+
+$root->loadById(3);
+echo $root;
+
+#Primeiro método (simples)
+##$sql = new Sql();
+##$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 //Mostra na tela como um Json
-echo json_encode($usuarios);
+##echo json_encode($usuarios);
 
 ?>
